69affc97c22375a2b515867309ea7d3f58bb2610
louyza <louyzadut@gmail.com>
2019-12-16 16:04:10 -0200

    ACABOU AEEEE

--- controller/programador_controller.py
+++ controller/programador_controller.py
@@ -18,11 +18,30 @@ class ProgramadoresController(Resource):
         id_db = request.json['id_db']
         id_framework = request.json['id_framework']
         id_linguagem = request.json['id_linguagem']
-        programador = Programador(
-            nome, id_framework=id_framework, id_db=id_db, id_linguagem=id_linguagem)
-        programador_id = self.dao.inserir(programador)
-        programador = self.dao.buscar_por_id(programador_id)
-        return programador
+
+        if nome.lower() == 'nicole' and id_db == 1 and id_framework == 1 and id_linguagem == 1:
+            programador = Programador(
+                nome.lower(), id_framework=id_framework, id_db=id_db, id_linguagem=id_linguagem)
+            programador_id = self.dao.inserir(programador)
+            programador = self.dao.buscar_por_id(programador_id)
+            return programador
+        
+        elif nome.lower() == 'mateus' and id_db == 2 and id_framework == 2 and id_linguagem == 2:
+            programador = Programador(
+                nome.lower(), id_framework=id_framework, id_db=id_db, id_linguagem=id_linguagem)
+            programador_id = self.dao.inserir(programador)
+            programador = self.dao.buscar_por_id(programador_id)
+            return programador
+        
+        elif nome.lower() == 'tiago' and id_db == 3 and id_framework == 3 and id_linguagem == 3:
+            programador = Programador(
+                nome.lower(), id_framework=id_framework, id_db=id_db, id_linguagem=id_linguagem)
+            programador_id = self.dao.inserir(programador)
+            programador = self.dao.buscar_por_id(programador_id)
+            return programador
+        
+        else:
+            return 'Combinação Inválida'        
 
     def put(self, id):
         id_body = request.json['id']
@@ -32,7 +51,7 @@ class ProgramadoresController(Resource):
         id_linguagem = request.json['id_linguagem']
 
         if id_body != id:
-            return 'Ids de rota e body diferentes, seu tatu'
+            return 'Ids de rota e body diferentes, seu tatu!'
         programador = Programador(
             nome, id_db=id_db, id_framework=id_framework, id_linguagem=id_linguagem, id=id)
         self.dao.alterar(programador)
@@ -41,4 +60,4 @@ class ProgramadoresController(Resource):
 
     def delete(self, id):
         self.dao.deletar(id)
-        return 'deletado meu'
+        return 'Deletado Meu'
